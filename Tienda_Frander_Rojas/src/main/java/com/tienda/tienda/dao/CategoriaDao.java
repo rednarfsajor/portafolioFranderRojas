b6f2e3fabbae6af044ed9dfbecff71b76619b46f
rednarfsajor <frojas20190@ufide.ac.cr>
2024-02-21 20:36:52 -0600

    Semana 6.0
    Categoría completa
    DB
    Controller
    Service
    Fragmentos
    Listado

---
+++ Tienda_Frander_Rojas/src/main/java/com/tienda/tienda/dao/CategoriaDao.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.tienda.dao;
+
+import com.tienda.tienda.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaDao extends JpaRepository <Categoria,Long> {
+    
+}
---
+++ Tienda_Frander_Rojas/src/main/java/com/tienda/tienda/service/CategoriaService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda.tienda.service;
+
+
+import com.tienda.tienda.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+    
+    // Se obtiene un listado de categorias en un List
+    public List<Categoria> getCategorias(boolean activos);
+ 
+}
