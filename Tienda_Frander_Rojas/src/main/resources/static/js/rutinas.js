72d0cccfff657ea54bd2b540ebbe3e493143e4c8
rednarfsajor <frojas20190@ufide.ac.cr>
2024-03-13 20:16:35 -0600

    Semana 9 (1)
    Parte 1 de la semana 9
    Entidad Producto
    Template.Pruebas
    (Traslado del proyecto de la profe)

--- Tienda_Frander_Rojas/src/main/resources/static/js/rutinas.js
+++ Tienda_Frander_Rojas/src/main/resources/static/js/rutinas.js
@@ -1,18 +1,22 @@
-function readURL(input){
-    if (input.files && input.files[0]){
+/* La siguiente función se utiliza para visualizar la imagen seleccionada en la
+ * página html donde se desea "cargar" utilizando un llamado "ajax"*/
+function readURL(input) {
+    if (input.files && input.files[0]) {
         var reader = new FileReader();
-        reader.onload = function(e){
-            $('blah')
-                    .attr('src',e.target.result)
+        reader.onload = function (e) {
+            $('#blah')
+                    .attr('src', e.target.result)
                     .height(200);
         };
         reader.readAsDataURL(input.files[0]);
     }
 }
 
-function addCard (formulario){
+/* La siguiente función se utiliza para activar la cantidad de elementos seleccionados
+ * En el carrito de compras utilizando un llamado "ajax" */
+function addCard(formulario) {
     var valor = formulario.elements[0].value;
-    var url='/carrito/agregar';
+    var url = '/carrito/agregar';
     url = url + '/' + valor;
-    $("resultadoBlock").load(url);
+    $("#resultsBlock").load(url);
 }
